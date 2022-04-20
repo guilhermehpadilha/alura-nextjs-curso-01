c81d9850d5d0bb9cebe6dd0b25ef8178d7df23f0
Guilherme Padilha <padilha.992@gmail.com>
2022-04-20 20:13:30 -0300

    adicionando FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,5 @@
-import Link from "../src/components/Link";
-import PageTitle from "../src/components/PageTitle";
+import FAQScreen from "../src/screens/FAQScreen";
+export default FAQScreen;
 
 export async function getStaticProps() {
 	const FAQ_API_URL =
@@ -17,39 +17,4 @@ export async function getStaticProps() {
 			faq,
 		},
 	};
-}
-
-export default function FaqPage({ faq }) {
-	// const [faq, setFaq] = useState([]);
-
-	// useEffect(() => {
-	// 	const FAQ_API_URL =
-	// 		"https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-	// 	fetch(FAQ_API_URL)
-	// 		.then((respostaDoServidor) => {
-	// 			return respostaDoServidor.json();
-	// 		})
-	// 		.then((resposta) => {
-	// 			setFaq(resposta);
-	// 		});
-	// }, []);
-
-	return (
-		<div>
-			<PageTitle>FAQ - Alura Cases</PageTitle>
-
-			<h1>Página de Perguntas - FAQ</h1>
-			<Link href='/'>Ir para a Home</Link>
-			<ul>
-				{faq.map(({ answer, question }) => (
-					<li key={question}>
-						<article>
-							<h2> {question} </h2>
-							<p> {answer} </p>
-						</article>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
 }
